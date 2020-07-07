aca39d4b74dc4954eb02f846634265c1fe069579
Vibhanshu Garg <v2001.garg@gmail.com>
2020-07-07 08:09:20 +0530

    Updates

---
+++ precache-manifest.bbd83206532efddcb9e19da7a70235c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a46da8d4283be21f4176e34ca0d93ff",
+    "url": "/food-recipe-react-app/index.html"
+  },
+  {
+    "revision": "ae037cb234ecd923db24",
+    "url": "/food-recipe-react-app/static/css/main.cc650c2f.chunk.css"
+  },
+  {
+    "revision": "5851b7f772394e47cc7d",
+    "url": "/food-recipe-react-app/static/js/2.abe1735d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/food-recipe-react-app/static/js/2.abe1735d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae037cb234ecd923db24",
+    "url": "/food-recipe-react-app/static/js/main.308f2f1d.chunk.js"
+  },
+  {
+    "revision": "3395539ab6fbb3a93bdb",
+    "url": "/food-recipe-react-app/static/js/runtime-main.1f3be0a5.js"
+  }
+]);
